50e4503279555fdb2d103609e3c12e960a81b3b8
EnanDorta <junior.goncalves01@hotmail.com>
2022-04-17 23:38:54 -0300

    update in code

--- src/hooks/useModalPassword.js
+++ src/hooks/useModalPassword.js
@@ -2,6 +2,9 @@ import { useState, useContext, createContext } from "react";
 import { useUsers } from "../hooks/useUsers";
 import { useChecked } from "../hooks/useChecked";
 import { api } from "../service/api";
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const UseModalPasswordContext = createContext({})
 
@@ -13,7 +16,6 @@ export function UseModalPasswordProvider({ children }) {
   // Context
   const { users, setReloadState } = useUsers()
   const { isChecked } = useChecked()
-
   /* Modal for change password */
   /* Open modal */
   function handleOpenNewChangePasswordModal() {
@@ -42,27 +44,26 @@ export function UseModalPasswordProvider({ children }) {
    const numbers = /[0-9]/
    const characterSpecial = /[@|#|%|*]/;
 
-   if (newPassword.includes(" ")) return alert('A senha não pode conter espaço')
+   if (newPassword.includes(" ")) return toast.error('A senha não pode conter espaço')
 
-   if (newPassword.length <= 9) return alert('Senha menor que 10 caracteres')
+   if (newPassword.length <= 9) return toast.error('Senha menor que 10 caracteres')
 
    if (!characterSpecial.test(newPassword)) {
-     return alert('A senha deve conter caracteres especiais')
+     return toast.error('A senha deve conter caracteres especiais')
    }
 
    if (!numbers.test(newPassword)) {
-     return alert('A senha deve conter algum número de 0 - 9')
+     return toast.error('A senha deve conter algum número de 0 - 9')
    }
 
    if (!smallLetters.test(newPassword)) {
-     return alert('A senha deve conter letras minúsculas')
+     return toast.error('A senha deve conter letras minúsculas')
    }
 
    if (!bigLetters.test(newPassword)) {
-     return alert('A senha deve conter letras maiúsculas')
+     return toast.error('A senha deve conter letras maiúsculas')
    }
    /* This reset password from users selected */
-   console.log(isChecked)
    isChecked.forEach((item, index) => {
      if (item === true) {
        api.patch(`users/${users[index].id}`, {"password": newPassword})   
